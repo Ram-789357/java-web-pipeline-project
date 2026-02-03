b831cde68635040e464e9154f72eba8dff1f375d
Ram-789357 <141727743+Ram-789357@users.noreply.github.com>
2026-02-03 16:31:42 +0530

    Update App.java

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Java Web Pipeline Project");
+        System.out.println("Java Web Pipeline Project by ram...");
     }
 }
